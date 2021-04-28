0dc290e59c48cda9a4a0a45e106ffe9b5eb50056
Brian Reiche <brian.reiche@gmail.com>
2021-04-28 15:55:50 -0400

    Initial commit of cuda project template.

---
+++ CudaTemplate/cudaMain.cu
@@ -0,0 +1,18 @@
+﻿
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <stdio.h>
+#include <assert.h>
+inline cudaError_t checkCuda(cudaError_t result, const char *error_step)
+{
+    if (result != cudaSuccess) {
+        printf("CUDA Step: %s\nCUDA Runtime Error: %s\n", error_step, cudaGetErrorString(result));
+        assert(result == cudaSuccess);
+    }
+    return result;
+}
+
+int main()
+{
+    return 0;
+}
